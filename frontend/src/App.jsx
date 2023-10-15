0ff4e81c4ecc750b1941654b827dbe1ac7120019
vanyason <vanyason@mail.ru>
2023-10-15 22:48:00 +0400

    Did frontend prototype + some files rearrangement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,43 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
-
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
-
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
-
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+  return (
+    <div className="h-screen flex flex-col bg-gradient-to-tr from-slate-900 to-fuchsia-900 text-cyan-200 font-mono">
+      <div className="com outline-sky-500 basis-1/2 flex">
+        <div className="com outline-orange-300 basis-2/3 flex items-center justify-center">
+          {" Bulb here "}
         </div>
-    )
+        <div className="com outline-orange-300 basis-1/3 flex items-center justify-center">
+          {" Button here "}
+        </div>
+      </div>
+
+      <div className="com outline-sky-500 basis-1/3 flex ">
+        <div className="com outline-orange-300 flex items-center justify-center basis-5/12">
+          {" Color picker "}
+        </div>
+        <div className="com outline-orange-300 flex items-center justify-center basis-5/12">
+          {" Temp picker picker "}
+        </div>
+        <div className="com outline-orange-300 flex items-center justify-center basis-2/12">
+          {" Bright picker "}
+        </div>
+      </div>
+
+      <div className="com outline-sky-500 basis-1/6 flex flex-col items-center justify-center gap-2">
+        <button
+          type="button"
+          className="com outline-orange-300 items-center ring ring-blue-300 hover:ring-blue-400 w-44"
+        >
+          {" pick interface "}
+        </button>
+        <button
+          type="button"
+          className="com outline-orange-300 items-center ring ring-blue-300 hover:ring-blue-400 w-44"
+        >
+          {" logs "}
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import './style.css'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./main.css";
 
-const container = document.getElementById('root')
-
-const root = createRoot(container)
+const container = document.getElementById("root");
+const root = createRoot(container);
 
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
