a076811e6777294af54d52eea1085e46ff47b80a
vanyason <vanyason@mail.ru>
2023-10-16 00:10:38 +0400

    New design idea...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,42 @@
 function App() {
   return (
     <div className="h-screen flex flex-col bg-gradient-to-tr from-slate-900 to-fuchsia-900 text-cyan-200 font-mono">
-      <div className="com outline-sky-500 basis-1/2 flex">
-        <div className="com outline-orange-300 basis-2/3 flex items-center justify-center">
-          {" Bulb here "}
+      {/* I section */}
+      <div className="com outline-sky-500 basis-7/12 flex">
+        <div className="com outline-orange-300 basis-1/2 flex flex-col items-center justify-around">
+          <div className="com outline-green-300 basis-5/6 flex items-center justify-center">
+            {" Bulb here "}
+          </div>
+          <button
+            type="button"
+            className="com outline-green-300 basis-1/6 ring ring-blue-300 hover:ring-blue-400 max-h-8 w-44"
+          >
+            {" on/off "}
+          </button>
         </div>
-        <div className="com outline-orange-300 basis-1/3 flex items-center justify-center">
-          {" Button here "}
+        <div className="com outline-orange-300 basis-1/2 flex items-center justify-center">
+          {" logs here "}
         </div>
       </div>
-
-      <div className="com outline-sky-500 basis-1/3 flex ">
+      {/* II section */}
+      <div className="com outline-sky-500 basis-4/12 flex justify-around">
         <div className="com outline-orange-300 flex items-center justify-center basis-5/12">
           {" Color picker "}
         </div>
         <div className="com outline-orange-300 flex items-center justify-center basis-5/12">
           {" Temp picker picker "}
         </div>
-        <div className="com outline-orange-300 flex items-center justify-center basis-2/12">
+        <div className="com outline-orange-300 flex items-center justify-center basis-1/12">
           {" Bright picker "}
         </div>
       </div>
-
-      <div className="com outline-sky-500 basis-1/6 flex flex-col items-center justify-center gap-2">
+      {/* III section */}
+      <div className="com outline-sky-500 basis-1/12 flex flex-col items-center justify-center gap-2">
         <button
           type="button"
           className="com outline-orange-300 items-center ring ring-blue-300 hover:ring-blue-400 w-44"
         >
           {" pick interface "}
-        </button>
-        <button
-          type="button"
-          className="com outline-orange-300 items-center ring ring-blue-300 hover:ring-blue-400 w-44"
-        >
-          {" logs "}
         </button>
       </div>
     </div>
