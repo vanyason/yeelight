1996e4c6654f3a3d0ad9e9eaad9c217dfec51a0f
vanyason <vanyason@mail.ru>
2023-10-22 19:15:22 +0400

    Added spinner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import BulbSVG from "./components/BulbSVG";
 import BulbToggle from "./components/BulbToggle";
 import LogsSection from "./components/LogsScetion";
+import { ClipLoader } from "react-spinners";
 
 function ColorPicker(props) {
   return <div {...props}>{" Color Picker "}</div>;
@@ -16,7 +17,7 @@ function BrightnessSlider(props) {
 
 function NetInterfacePicker(props) {
   return (
-    <button type="button" className="com outline-orange-300 items-center ring ring-blue-300 hover:ring-blue-400 w-44">
+    <button type="button" className="com outline-orange-300 items-center ring ring-blue-300 hover:ring-blue-400 sm:w-20 sm:h-10 w-20 h-6">
       {" Pick Net interface "}
     </button>
   );
@@ -27,20 +28,17 @@ export default function App() {
     <div className="h-screen flex flex-col bg-gradient-to-br from-violet-900 to-slate-900 text-sky-50">
       <div className="com outline-sky-500 basis-7/12 flex overflow-auto">
         <div className="com outline-orange-300 basis-2/5 flex flex-col items-center justify-around">
-          <BulbSVG className="com outline-green-300 basis-5/6 flex items-center justify-center" />
-          <BulbToggle />
+          <BulbSVG className="com outline-green-300 basis-8/12 flex items-center justify-center" color="#808080" />
+          <ClipLoader className="absolute" color="#36d7b7" speedMultiplier="0.7" />
+          {/* <BulbToggle />
+          <NetInterfacePicker className="com outline-orange-300 items-center ring ring-blue-300 hover:ring-blue-400 " /> */}
         </div>
         <LogsSection className="com p-4 outline-orange-300 basis-3/5 overflow-auto font-mono bg-gradient-to-b from-indigo-900 to-slate-900 ring-offset-2 ring" />
       </div>
-
-      <div className="com outline-sky-500 basis-4/12 flex justify-around">
+      <div className="com outline-sky-500 basis-5/12 flex justify-around">
         <ColorPicker className="com outline-orange-300 flex items-center justify-center basis-5/12" />
         <TemperaturePicker className="com outline-orange-300 flex items-center justify-center basis-5/12" />
         <BrightnessSlider className="com outline-orange-300 flex items-center justify-center basis-1/12" />
-      </div>
-
-      <div className="com outline-sky-500 basis-1/12 flex flex-col items-center justify-center gap-2">
-        <NetInterfacePicker className="com outline-orange-300 items-center ring ring-blue-300 hover:ring-blue-400 w-44" />
       </div>
     </div>
   );
